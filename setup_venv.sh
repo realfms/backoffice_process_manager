d4edb44ec7f21c5fed74a2c7c8a6e5e96891aa01
Carlos David Rodríguez Peña <cdavidrp@gmail.com>
2013-02-11 12:33:03 +0100

    Modify instalation script for compatibility between pip versions

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-echo Installing the package manager 'pip'
-sudo apt-get install -y python-pip python-dev build-essential 
+echo Installing generic python requirements...
+sudo apt-get install -y python-dev python-pip build-essential 
 RETVAL=$?
 [ $RETVAL -eq 0 ] && (echo; echo Success)
-[ $RETVAL -ne 0 ] && (echo; echo Failed to install pip; exit 1)
+[ $RETVAL -ne 0 ] && (echo; echo Failed to install python requirements; exit 1)
 echo
 
-echo Installing the virtualenv program
-sudo pip install virtualenv --upgrade
+echo Installing the virtualenv program...
+sudo pip install virtualenv
 RETVAL=$?
 [ $RETVAL -eq 0 ] && (echo; echo Success)
 [ $RETVAL -ne 0 ] && (echo; echo Failed to install virtualenv; exit 1)
+echo
+
+echo Installing postgresql...
+sudo apt-get install -y postgresql postgresql-server-dev-all
+RETVAL=$?
+[ $RETVAL -eq 0 ] && (echo; echo Success)
+[ $RETVAL -ne 0 ] && (echo; echo Failed to install postgresql; exit 1)
 echo
 
 echo Creating virtual environment...
@@ -19,17 +26,12 @@ virtualenv venv
 RETVAL=$?
 [ $RETVAL -eq 0 ] && (echo; echo Success)
 [ $RETVAL -ne 0 ] && (echo; echo Failed to create the environment; exit 1)
-echo
-
-echo Installing postgresql, may ask for password
-sudo apt-get install -y postgresql postgresql-server-dev-all
-RETVAL=$?
-[ $RETVAL -eq 0 ] && (echo; echo Success)
-[ $RETVAL -ne 0 ] && (echo; echo Failed to install postgresql; exit 1)
+. venv/bin/activate
 echo
 
 echo Installing requirements from requirements.txt...
-sudo pip install -E venv -r ./requirements.txt
+cd venv
+sudo pip install -r ../requirements.txt
 RETVAL=$?
 [ $RETVAL -eq 0 ] && (echo; echo Success)
 [ $RETVAL -ne 0 ] && (echo; echo Failed to install the requirements; exit 1)

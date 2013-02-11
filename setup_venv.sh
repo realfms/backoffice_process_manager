baa1ba24871983fa7a5c2a41abc3170c3ae67543
Alberto Blázquez Rodríguez <hola@albertoblazquez.net>
2013-02-11 11:59:42 +0100

    Change script to join commands and fix some defects

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
 
-echo Installing the virtualenv program
-pip install virtualenv
+echo Installing the package manager 'pip'
+sudo apt-get install -y python-pip python-dev build-essential 
 RETVAL=$?
 [ $RETVAL -eq 0 ] && (echo; echo Success)
-[ $RETVAL -ne 0 ] && (echo; echo Failed to install virtualenv)
+[ $RETVAL -ne 0 ] && (echo; echo Failed to install pip; exit 1)
+echo
+
+echo Installing the virtualenv program
+sudo pip install virtualenv --upgrade
+RETVAL=$?
+[ $RETVAL -eq 0 ] && (echo; echo Success)
+[ $RETVAL -ne 0 ] && (echo; echo Failed to install virtualenv; exit 1)
 echo
 
 echo Creating virtual environment...
 virtualenv venv
 RETVAL=$?
 [ $RETVAL -eq 0 ] && (echo; echo Success)
-[ $RETVAL -ne 0 ] && (echo; echo Failed to create the environment)
+[ $RETVAL -ne 0 ] && (echo; echo Failed to create the environment; exit 1)
 echo
 
 echo Installing postgresql, may ask for password
-sudo apt-get install -y postgresql
+sudo apt-get install -y postgresql postgresql-server-dev-all
 RETVAL=$?
 [ $RETVAL -eq 0 ] && (echo; echo Success)
-[ $RETVAL -ne 0 ] && (echo; echo Failed to install postgresql)
-echo
-
-echo Installing postgresql-server, may ask for password
-sudo apt-get install -y postgresql-server-dev-all
-RETVAL=$?
-[ $RETVAL -eq 0 ] && (echo; echo Success)
-[ $RETVAL -ne 0 ] && (echo; echo Failed to install postgresql-server)
-echo
-
-echo Installing the python-dev package
-sudo apt-get install -y python-dev
-RETVAL=$?
-[ $RETVAL -eq 0 ] && (echo; echo Success)
-[ $RETVAL -ne 0 ] && (echo; echo Failed to install python-dev)
+[ $RETVAL -ne 0 ] && (echo; echo Failed to install postgresql; exit 1)
 echo
 
 echo Installing requirements from requirements.txt...
-pip install -E venv -r ./requirements.txt
+sudo pip install -E venv -r ./requirements.txt
 RETVAL=$?
 [ $RETVAL -eq 0 ] && (echo; echo Success)
-[ $RETVAL -ne 0 ] && (echo; echo Failed to install the requirements)
+[ $RETVAL -ne 0 ] && (echo; echo Failed to install the requirements; exit 1)
 echo

c9afee0637b9af4ee06207eba4bdc7a2cc18969a
Alberto Blázquez Rodríguez <hola@albertoblazquez.net>
2013-02-14 13:02:05 +0100

    Fix error showing subprocesses and implemented tasks animations

--- static/html/js/handlers.js
+++ static/html/js/handlers.js
@@ -1,15 +1,38 @@
 
 $(function() {
 
-var details = $(".detail");
-var articles = $(".subprocess");
+    /* Global Vars */
 
-function detailHandler(e) {
-	var index = $(this).parent.index();
-	articles.addClass("hidden");
-	articles.eq(index).removeClass("hidden");
-}
+    var details  = $('.detail');
+    var articles = $('.subprocess');
+    var moreInfo = $('.more-info');
 
-details.on('click', detailHandler);
+
+    /* Handlers */
+
+    var detailHandler = function() {
+    	var index = $(this).parent().index();
+    	articles.addClass("hidden").eq(index).removeClass("hidden");
+    }
+
+    var externalResultsHandler = function() {
+        var chevron = $(this).children();
+        chevron.toggleClass('icon-chevron-down icon-chevron-up');
+
+        var index = $(this).parent().index();
+
+        var visibleArticle = articles.filter(function(i) {
+            return !articles.eq(i).hasClass('hidden');
+        })
+
+        var rows = visibleArticle.find('.tasks').children();
+        rows.eq(index + 1).fadeToggle();
+    }
+
+
+    /* Event Assignments */
+
+    details.on('click', detailHandler);
+    moreInfo.on('click', externalResultsHandler);
 
 });

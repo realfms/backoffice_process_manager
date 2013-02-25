ce11d7c7590ed7c9d78c78dd0372181976c01ddd
Salvador Perez Martin <sisekeom@gmail.com>
2013-02-25 11:31:42 +0100

    Add cache copy of the files

--- static_cache/js/handlers.js
+++ static_cache/js/handlers.js
@@ -8,23 +8,19 @@ $(function() {
     var moreInfo = $('.more-info');
 
     var currentArticle;
+    var pid = 0;
     var HIDDEN = 'hidden';
 
 
     /* Handlers */
 
     var processHandler = function() {
-        var TIME = 500;
-        var BLUE = 'blue';
+        var row = $(this);
+        currentArticle = row.index();
 
-        var row  = $(this);
-    	currentArticle = row.index();
+        pid = parseInt( row.find('.pid').text() );
 
-        articles.addClass( HIDDEN );
-        articles.eq( currentArticle ).removeClass( HIDDEN );
-
-        processes.removeClass( BLUE );
-        row.addClass( BLUE );
+        toggleSelectedProcess( row );
     };
 
 
@@ -52,11 +48,99 @@ $(function() {
     };
 
 
+    var refreshHandler = function() {
+
+        var url = window.location.href;
+        var i = url.lastIndexOf('pid');
+
+        if ( i == -1 ) {
+            window.location.href += ("?pid=" + pid);
+
+        } else {
+            var first = url.substring(0, i);
+            var last  = url.substring(i, i+4) + pid + url.substring(i+5, url.length);
+
+            window.location.href = first + last;
+        }
+
+    };
+
+
+    var searchSelectedProcessHandler = function() {
+        pid = parseInt( getParam('pid') );
+
+        var selectedRow = processes.filter(function(i) {
+            var column = processes.eq(i).find('.pid')
+            var rowPID = parseInt( column.text() );
+            return rowPID === pid;
+        });
+
+        currentArticle = proc.index();
+
+        toggleSelectedProcess( selectedRow );
+    };
+
+
+    /* Auxiliary Functions */
+
+    var toggleSelectedProcess = function(row) {
+        var BLUE = 'blue';
+
+        articles.addClass( HIDDEN );
+        articles.eq( currentArticle ).removeClass( HIDDEN );
+
+        processes.removeClass( BLUE );
+        row.addClass( BLUE );
+    };
+
+
+    var getParam = function(param) {
+        var params = window.location.search.slice(1);
+        var i = params.indexOf('=');
+        var p = params.substring(0, i);
+
+        var res = "";
+
+        // Loop to search every param
+
+        if (p == param) {
+            var last = params.indexOf('&');
+            if (last === -1) {
+                last = params.length;
+            }
+
+            res = params.substring(i+1, last);
+        }
+
+        return res;
+    };
+
+    /*
+    var getURLParameter = function (sParam) {
+        var sPageURL = window.location.search.slice(1);
+        var sURLVariables = sPageURL.split('&');
+
+        for (var i = 0; i < sURLVariables.length; i++) {
+            var sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] == sParam) {
+                return sParameterName[1];
+            }
+        }
+
+        return "";
+    }​;
+    /
+
+
     /* Event Assignments */
 
     processes.on('click', processHandler);
     moreInfo.on('click', externalResultsHandler);
+
+    $('#refresh').on('click', refreshHandler);
     $('.select').on('change', subprocessHandler);
+    $('window').on('load', searchSelectedProcessHandler);
 
 
     /* Initialization */

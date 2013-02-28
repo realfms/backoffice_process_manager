fa71a0d63f6f921ed811a9fd47fe80425303d959
Alberto Blázquez Rodríguez <hola@albertoblazquez.net>
2013-02-28 10:19:46 +0100

    Test SDR Generation

--- processes/pdf/invoice.py
+++ processes/pdf/invoice.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
-# coding=utf-8 
+# coding=utf-8
 
 """
 Copyright 2012 Telefonica Investigación y Desarrollo, S.A.U
 
 This file is part of Billing_PoC.
 
-Billing_PoC is free software: you can redistribute it and/or modify it under the terms 
-of the GNU Affero General Public License as published by the Free Software Foundation, either 
+Billing_PoC is free software: you can redistribute it and/or modify it under the terms
+of the GNU Affero General Public License as published by the Free Software Foundation, either
 version 3 of the License, or (at your option) any later version.
-Billing_PoC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even 
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero 
+Billing_PoC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License along with Billing_PoC. 
+You should have received a copy of the GNU Affero General Public License along with Billing_PoC.
 If not, see http://www.gnu.org/licenses/.
 
 For those usages not covered by the GNU Affero General Public License please contact with::mac@tid.es
-"""  
+"""
 
 '''
 Created on 10/10/2012
@@ -45,25 +45,25 @@ def compute_invoice_details ():
            }
 
 def generate_pdf_and_upload(invoice_json):
-    
+
     # Adding ".pdf" fo the name of the SDR file
     file_name = "{0}.pdf".format(invoice_json['sdr_file_name'])
-    
+
     invoice_json['invoice'] = compute_invoice_details()
-    
+
     pisa.showLogging()
-    
+
     with codecs.open(TEMPLATE_PATH, 'r', 'utf-8') as f:
         template_content = f.read()
-        
+
     template = Template(template_content)
-    
+
     with open (file_name, "wb") as f:
         pdf = pisa.CreatePDF(template.render(invoice_json), f)
-    
-    if not pdf.err:                             
+
+    if not pdf.err:
         upload_invoice_to_s3(file_name)
-        
+
     invoice_json['pdf_file_name'] = file_name
 
     return (invoice_json, None)
--- processes/tests.py
+++ processes/tests.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
-#coding=utf-8 
+#coding=utf-8
 
 """
 Copyright 2012 Telefonica Investigacion y Desarrollo, S.A.U
 
 This file is part of Billing_PoC.
 
-Billing_PoC is free software: you can redistribute it and/or modify it under the terms 
-of the GNU Affero General Public License as published by the Free Software Foundation, either 
+Billing_PoC is free software: you can redistribute it and/or modify it under the terms
+of the GNU Affero General Public License as published by the Free Software Foundation, either
 version 3 of the License, or (at your option) any later version.
-Billing_PoC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even 
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero 
+Billing_PoC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License along with Billing_PoC. 
+You should have received a copy of the GNU Affero General Public License along with Billing_PoC.
 If not, see http://www.gnu.org/licenses/.
 
 For those usages not covered by the GNU Affero General Public License please contact with::mac@tid.es
-"""  
+"""
 
 """
 Created on 16/01/2013
@@ -35,6 +35,8 @@ from django.test import TestCase
 from customer.salesforce import get_customer_details_from_sf
 from common.salesforce.salesforce import update_contact
 
+from processes.sdr_gen import gen_sdr
+
 class TestGenerator(TestCase):
 
     def test_salesforce_update_contact(self):
@@ -48,3 +50,9 @@ class TestGenerator(TestCase):
         result = get_customer_details_from_sf('003d000000kC2JHAA0')
 
         print result
+
+
+class TestSDR(TestCase):
+
+    def test_sdr(self):
+        gen_sdr()

db48d264dab4a199d946e2afca2909c9f480b9c9
EC2 Default User <ec2-user@ip-10-252-32-194.us-west-2.compute.internal>
2013-08-12 07:44:16 +0000

    Configured mysql service

--- system-dependencies.sh
+++ system-dependencies.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 sudo yum -y install make glibc-devel gcc gcc-c++ openssl-devel libxml2 libxml2-devel python27-devel mysql-devel
-sudo yum -y install mysql
+sudo yum -y install mysql mysql-server
+
+sudo /sbin/chkconfig mysqld on
+sudo /sbin/service mysqld start
+
+#mysqladmin -u root password 'new-password'
